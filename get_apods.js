38fa091b541eeef74235fd300bb189ef9e7c294c
Samarth Kishor <samarthkishor1@gmail.com>
2017-12-28 00:06:21 -0500

    IT WORKS

--- get_apods.js
+++ get_apods.js
@@ -1,6 +1,6 @@
 const Nightmare = require('nightmare');
 const nightmare = Nightmare();
-const vo = require('vo');
+// const vo = require('vo');
 const fs = require('fs');
 
 const data = fs.readFileSync('dates.csv', 'utf-8');
@@ -17,68 +17,13 @@ function getExplanation(date) {
   };
 }
 
-function getPicture(date) {
-  return nightmare => {
-    nightmare
-      .evaluate(() => {
-        return document.querySelector(
-          'body > center:nth-child(1) > p:nth-child(3) > a > img'
-        );
-      })
-      .then(result => {
-        if (result !== null) {
-          // check if the page contains an image, else assume it contains an embedded video
-          return nightmare
-            .click('body > center:nth-child(1) > p:nth-child(3) > a > img')
-            .wait(5000)
-            .screenshot(`./lib/pictures/${date}.jpg`)
-            .then(() => {
-              console.log(`The picture ${date}.jpg has been saved.`);
-            })
-            .catch(error => {
-              console.error(error);
-            });
-        } else {
-          console.log('The media is an embedded video, not an image.');
-          return nightmare
-            .wait('body > center:nth-child(1) > p:nth-child(3) > iframe')
-            .wait(3000) // wait for the video to render
-            .evaluate(() => {
-              return document
-                .querySelector(
-                  'body > center:nth-child(1) > p:nth-child(3) > iframe'
-                )
-                .getAttribute('src');
-            })
-            .then(result => {
-              return nightmare
-                .goto(result)
-                .wait(3000) // wait for the video to enter fullscreen mode
-                .screenshot(`./lib/pictures/${date}.jpg`)
-                .then(() => {
-                  console.log(`The picture ${date}.jpg has been saved.`);
-                })
-                .catch(error => {
-                  console.error(error);
-                });
-            })
-            .catch(error => {
-              console.error(error);
-            });
-        }
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  };
-}
-
 console.log('Saving explanations and images...');
 
 dates
   .reduce((accumulator, date) => {
     return accumulator.then(() => {
       return nightmare.use(getExplanation(date)).then(result => {
+        // Save the explanation to explanations.json
         const entry = {
           date: date,
           picture: `./lib/pictures/${date}.jpg`,
@@ -97,109 +42,62 @@ dates
           }
         );
 
-        return nightmare.use(
-          getPicture(date).catch(error => console.error(error))
-        );
+        return nightmare
+          .evaluate(() => {
+            return document.querySelector(
+              'body > center:nth-child(1) > p:nth-child(3) > a > img'
+            );
+          })
+          .then(result => {
+            // check if the page contains an image, else assume it contains an embedded video
+            if (result !== null) {
+              return nightmare
+                .click('body > center:nth-child(1) > p:nth-child(3) > a > img')
+                .wait(5000)
+                .screenshot(`./lib/pictures/${date}.jpg`)
+                .then(() => {
+                  console.log(`The picture for ${date} has been saved.`);
+                })
+                .catch(error => {
+                  console.error(error);
+                });
+            } else {
+              return nightmare
+                .wait('body > center:nth-child(1) > p:nth-child(3) > iframe')
+                .wait(3000) // wait for the video to render
+                .evaluate(() => {
+                  return document
+                    .querySelector(
+                      'body > center:nth-child(1) > p:nth-child(3) > iframe'
+                    )
+                    .getAttribute('src');
+                })
+                .then(result => {
+                  console.log(
+                    `The media for ${date} is an embedded video, not an image.`
+                  );
+                  return nightmare
+                    .goto(result)
+                    .wait(3000) // wait for the video to enter fullscreen mode
+                    .screenshot(`./lib/pictures/${date}.jpg`)
+                    .then(() => {
+                      console.log(`The picture for ${date} has been saved.`);
+                    })
+                    .catch(error => {
+                      console.error(error);
+                    });
+                })
+                .catch(error => {
+                  console.error(error);
+                });
+            }
+          })
+          .catch(error => {
+            console.error(error);
+          });
       });
     });
   }, Promise.resolve())
   .then(() => {
     nightmare.end(() => console.log('...done'));
   });
-
-// dates.reduce((accumulator, date) => {
-//   return accumulator.then(() => {
-//     return nightmare
-//       .goto('https://apod.nasa.gov/apod/ap' + date.toString() + '.html')
-//       .wait('body > p')
-//       .evaluate(() => {
-//         return document.querySelector('body > p').textContent.trim();
-//       })
-//       .then(result => {
-//         const entry = {
-//           date: date,
-//           picture: `./lib/pictures/${date}.jpg`,
-//           explanation: result.replace(/\n|Explanation:/g, ' ')
-//         };
-
-//         fs.readFile(
-//           './lib/explanations/explanations.json',
-//           'utf8',
-//           (error, data) => {
-//             if (error) throw error;
-//             let jsonData = JSON.parse(data);
-//             console.log(jsonData.elements);
-//             jsonData.elements.push(entry);
-//             fs.writeFile(
-//               './lib/explanations/explanations.json',
-//               JSON.stringify(jsonData, null, 4),
-//               error => {
-//                 if (error) throw error;
-//                 console.log('The explanation has been saved.');
-//               }
-//             );
-
-//             return nightmare
-//               .evaluate(() => {
-//                 return document.querySelector(
-//                   'body > center:nth-child(1) > p:nth-child(3) > a > img'
-//                 );
-//               })
-//               .then(result => {
-//                 if (result !== null) {
-//                   // check if the page contains an image, else assume it contains an embedded video
-//                   return nightmare
-//                     .click(
-//                       'body > center:nth-child(1) > p:nth-child(3) > a > img'
-//                     )
-//                     .wait(5000)
-//                     .screenshot(`./lib/pictures/${date}.jpg`)
-//                     .then(() => {
-//                       console.log(`The picture ${date}.jpg has been saved.`);
-//                     })
-//                     .catch(error => {
-//                       console.error(error);
-//                     });
-//                 } else {
-//                   console.log('The media is an embedded video, not an image.');
-//                   return nightmare
-//                     .wait(
-//                       'body > center:nth-child(1) > p:nth-child(3) > iframe'
-//                     )
-//                     .wait(3000) // wait for the video to render
-//                     .evaluate(() => {
-//                       return document
-//                         .querySelector(
-//                           'body > center:nth-child(1) > p:nth-child(3) > iframe'
-//                         )
-//                         .getAttribute('src');
-//                     })
-//                     .then(result => {
-//                       return nightmare
-//                         .goto(result)
-//                         .wait(3000) // wait for the video to enter fullscreen mode
-//                         .screenshot(`./lib/pictures/${date}.jpg`)
-//                         .then(() => {
-//                           console.log(
-//                             `The picture ${date}.jpg has been saved.`
-//                           );
-//                         })
-//                         .catch(error => {
-//                           console.error(error);
-//                         });
-//                     })
-//                     .catch(error => {
-//                       console.error(error);
-//                     });
-//                 }
-//               })
-//               .catch(error => {
-//                 console.error(error);
-//               });
-//           }
-//         );
-//       });
-//   });
-// }, Promise.resolve([]));
-
-// // nightmare.end();
