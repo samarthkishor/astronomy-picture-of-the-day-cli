7fa46c9cb2f2b37168b718ef5649287d01ecb1aa
Samarth Kishor <samarthkishor1@gmail.com>
2018-01-03 21:18:17 -0500

    Work pls

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='Astronomy Picture of the Day',
+    version='1.0',
+    description='',
+    author='Samarth Kishor',
+    packages=['pylatex', 'Naked'])
